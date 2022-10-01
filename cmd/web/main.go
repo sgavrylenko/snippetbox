2d56709eaf72526249b63e9e4b712ad2c046145b
Sergiy Gavrylenko <noreply>
2022-10-01 22:01:42 +0300

    ch_2.8

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,9 +7,13 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("../../ui/static/"))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
